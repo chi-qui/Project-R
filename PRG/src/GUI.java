3830eb9a163d932cbd166f0a4f008e12a13f64ee
Douglas <159087442+chi-qui@users.noreply.github.com>
2024-09-30 18:25:56 -0700

    Created a 'commence button', and added confirmation in the console when being pressed.

--- PRG/src/GUI.java
+++ PRG/src/GUI.java
@@ -1,20 +1,42 @@
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class GUI {
-    public static void main() {
+    public static void main(){
 
         JFrame frame = new JFrame();
+        frame.setLayout(new BorderLayout());
 
-        ImageIcon titleCard = new ImageIcon("C:\\Users\\micha\\Documents\\GitHub\\Project-R\\PRG\\Sprites\\title_concept_0.png");
-        JLabel label0 = new JLabel(titleCard);
-        label0.setIcon(titleCard);
-        label0.setText("Welcome");
-        label0.setHorizontalTextPosition(SwingConstants.TRAILING);
+
+        // Title Icon
+        ImageIcon titleIcon = new ImageIcon("C:\\Users\\micha\\Documents\\GitHub\\Project-R\\PRG\\src\\Sprites\\title_concept_0.png");
+        JLabel titleLabel = new JLabel(titleIcon);
+        titleLabel.setIcon(titleIcon);
+        titleLabel.setText("Welcome");
+
+        ImageIcon cbIcon = new ImageIcon("C:\\Users\\micha\\Documents\\GitHub\\Project-R\\PRG\\src\\Sprites\\commence_button.png");
+        Image cbIconImage = cbIcon.getImage();
+        Image cbiiScaled = cbIconImage.getScaledInstance(200, 100, Image.SCALE_SMOOTH);
+        ImageIcon scaledCbIcon = new ImageIcon(cbiiScaled);
+        JButton commenceButton = new JButton(scaledCbIcon);
+        commenceButton.setBorderPainted(false); // Removes border
+        commenceButton.setFocusPainted(false); // Removes Ugly Grey Square
+
+        commenceButton.addActionListener(new ActionListener(){
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("Commence Button Pressed");
+            }
+        });
 
         frame.setTitle("PRG");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(800, 600);
+        frame.add(titleLabel, BorderLayout.CENTER);
+        frame.add(commenceButton, BorderLayout.SOUTH);
+
         frame.setVisible(true);
-        frame.add(label0);
     }
 }
